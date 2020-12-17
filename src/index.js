6d0de3603745d3679e64d5405cce9b67e0cdba3f
opeyemibami <bamigbadeopeyemi@gmail.com>
2020-12-17 09:59:05 +0100

    templating in react component using js

--- src/index.js
+++ src/index.js
@@ -4,42 +4,37 @@ import ReactDom from 'react-dom'
 // CSS 
 import './index.css'
 
+const firstbook1 = {
+  img_url: "https://images-na.ssl-images-amazon.com/images/I/91uwocAMtSL._AC_UL480_SR315,480_.jpg",
+  author: 'Barrack Obama',
+  title: 'A promised land'
+}
+const firstbook2 = {
+  img_url:
+    "https://images-na.ssl-images-amazon.com/images/I/81RDxgZUHJL._AC_UL480_SR312,480_.jpg",
+  author: "Yhemmy Yhemmy",
+  title: "Stick to the trajectory",
+};
+
 function BookList(){
   return (
-    <section className='booklist'>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+    <section className="booklist">
+      <Book img_url = {firstbook1.img_url} title ={firstbook1.title} author={firstbook1.author}/>
+      <Book img_url = {firstbook2.img_url} title ={firstbook2.title} author={firstbook2.author}/>
       <Book />
     </section>
-  )
+  );
 }
 
-const Book = ()=>{
-  return(
-    <article>
-      <Image/>
-      <Title/>
-      <Author/>
-    </article>
-)
-
-}
-
-const Image = ()=>{
+const Book = (props) => {
   return (
-  <img src="https://images-na.ssl-images-amazon.com/images/I/91uwocAMtSL._AC_UL480_SR315,480_.jpg" alt=""/>
-  )
-}
-const Title = ()=>{
-  return <h1>A promised land</h1>
-}
-const Author = ()=>{
-  return <h1>Barrack Obama</h1>
-}
+    <article>
+      <img src={props.img_url} alt="" />
+      <h1>{props.title}</h1>
+      <h1>{props.author}</h1>
+    </article>
+  );
+};
 
 
 ReactDom.render(<BookList />,document.getElementById('root'));
