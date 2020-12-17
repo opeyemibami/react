f01ce39fd049fb78973ec44cc93412904498f87c
opeyemibami <bamigbadeopeyemi@gmail.com>
2020-12-17 13:15:50 +0100

    fix unique key error for props

--- src/index.js
+++ src/index.js
@@ -6,18 +6,21 @@ import './index.css'
 
 const books = [
   {
+    id: 1,
     img_url:
       "https://images-na.ssl-images-amazon.com/images/I/91uwocAMtSL._AC_UL480_SR315,480_.jpg",
     author: "Barrack Obama",
     title: "A promised land",
   },
   {
+    id: 2,
     img_url:
       "https://images-na.ssl-images-amazon.com/images/I/81RDxgZUHJL._AC_UL480_SR312,480_.jpg",
     author: "Yhemmy Yhemmy",
     title: "Stick to the trajectory",
   },
   {
+    id: 3,
     img_url:
       "https://images-na.ssl-images-amazon.com/images/I/81RDxgZUHJL._AC_UL480_SR312,480_.jpg",
     author: "Yhemmy Yhemmy",
@@ -33,7 +36,7 @@ function BookList(){
     {books.map((book)=>{
       const{img_url, author, title} = book;
       return(
-        <Book book ={book}/>
+        <Book key={id} book ={book}/>
       )
     })}
   </section>
