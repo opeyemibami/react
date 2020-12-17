6eec23d9614f1c21b03b3f2b39740d4b577e11e1
opeyemibami <bamigbadeopeyemi@gmail.com>
2020-12-17 13:13:11 +0100

    iterate through props

--- src/index.js
+++ src/index.js
@@ -4,40 +4,49 @@ import ReactDom from 'react-dom'
 // CSS 
 import './index.css'
 
-const firstbook1 = {
-  img_url: "https://images-na.ssl-images-amazon.com/images/I/91uwocAMtSL._AC_UL480_SR315,480_.jpg",
-  author: 'Barrack Obama',
-  title: 'A promised land'
-}
-const firstbook2 = {
-  img_url:
-    "https://images-na.ssl-images-amazon.com/images/I/81RDxgZUHJL._AC_UL480_SR312,480_.jpg",
-  author: "Yhemmy Yhemmy",
-  title: "Stick to the trajectory",
-};
+const books = [
+  {
+    img_url:
+      "https://images-na.ssl-images-amazon.com/images/I/91uwocAMtSL._AC_UL480_SR315,480_.jpg",
+    author: "Barrack Obama",
+    title: "A promised land",
+  },
+  {
+    img_url:
+      "https://images-na.ssl-images-amazon.com/images/I/81RDxgZUHJL._AC_UL480_SR312,480_.jpg",
+    author: "Yhemmy Yhemmy",
+    title: "Stick to the trajectory",
+  },
+  {
+    img_url:
+      "https://images-na.ssl-images-amazon.com/images/I/81RDxgZUHJL._AC_UL480_SR312,480_.jpg",
+    author: "Yhemmy Yhemmy",
+    title: "Stick to the trajectory",
+  },
+];
+
+
 
 function BookList(){
   return (
-    <section className="booklist">
-      <Book img_url = {firstbook1.img_url} title ={firstbook1.title} author={firstbook1.author}>
-        <p>
-          using children props Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus nam delectus dignissimos voluptates suscipit accusamus possimus, quidem porro optio iure!
-        </p>
-      </Book>
-      <Book img_url = {firstbook2.img_url} title ={firstbook2.title} author={firstbook2.author}/>
-      <Book />
-    </section>
-  );
+  <section className="booklist">
+    {books.map((book)=>{
+      const{img_url, author, title} = book;
+      return(
+        <Book book ={book}/>
+      )
+    })}
+  </section>
+  )
 }
 
 const Book = (props) => {
-  const {img_url,title,author,children} = props //destructuring the props 
+  const {img_url,title,author} = props.book //destructuring the props
   return (
     <article>
       <img src={img_url} alt="" />
       <h1>{title}</h1>
       <h1>{author}</h1>
-      {children} 
     </article>
   );
 };
